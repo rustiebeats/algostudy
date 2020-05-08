1fbbcc7be4324573459284aae088ae613fb0ad47
rustiebeats <59158271+rustiebeats@users.noreply.github.com>
2020-05-08 16:46:22 +0900

    군대가기전에풀고만다시발

---
+++ rpgextreme.cpp
@@ -0,0 +1,22 @@
+#define _DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+////
+
+
+int main(){
+#ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
